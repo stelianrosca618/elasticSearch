509eb31c7d015d80277a06c7af53def2699b471a
stelianrosca618 <stelianrosca618@outlook.com>
2024-03-14 18:36:56 +0200

    update UI logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,82 @@
-import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
 import React from "react";
+
+import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
+
 import {
   ErrorBoundary,
   Facet,
   SearchProvider,
   SearchBox,
   Results,
+  Result,
   PagingInfo,
   ResultsPerPage,
   Paging,
+  Sorting,
   WithSearch
 } from "@elastic/react-search-ui";
-import {
-  BooleanFacet,
-  Layout,
-  SingleLinksFacet,
-  SingleSelectFacet
-} from "@elastic/react-search-ui-views";
+import { SearchResult } from "@elastic/search-ui";
+import { Layout } from "@elastic/react-search-ui-views";
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
-import { SearchDriverOptions } from "@elastic/search-ui";
+
+import {
+  buildAutocompleteQueryConfig,
+  buildFacetConfigFromConfig,
+  buildSearchOptionsFromConfig,
+  buildSortOptionsFromConfig,
+  getConfig,
+  getFacetFields
+} from "./config/config-helper";
+const getFactFields = getFacetFields();
+const { hostIdentifier, searchKey, endpointBase, engineName } = getConfig();
 const connector = new AppSearchAPIConnector({
-  searchKey: "search-cu83qvk1txaefq3t7q1isfa2",
-  engineName: "national-parks-demo",
-  endpointBase: "https://582c5f420b5c4377a37008172eb74e25.us-central1.gcp.cloud.es.io:443"
+  searchKey,
+  engineName,
+  hostIdentifier,
+  endpointBase
 });
-const config: SearchDriverOptions = {
-  alwaysSearchOnInitialLoad: true,
-  apiConnector: connector,
-  hasA11yNotifications: true,
+const config = {
   searchQuery: {
-    result_fields: {
-      title: { raw: {} }
-    },
-    search_fields: {},
-    disjunctiveFacets: [""],
-    facets: {}
-  }
+    facets: buildFacetConfigFromConfig(),
+    ...buildSearchOptionsFromConfig()
+  },
+  autocompleteQuery: buildAutocompleteQueryConfig(),
+  apiConnector: connector,
+  alwaysSearchOnInitialLoad: true
 };
+const resultClicked = () => {
+  alert('resultView')
+}
+const CustomResultView = ({
+  result,
+  onClickLink
+}: {
+  result: SearchResult;
+  onClickLink: () => void;
+}) => (
+  <li className="sui-result">
+    <div className="sui-result__header">
+      <h3>
+        {/* Maintain onClickLink to correct track click throughs for analytics*/}
+        <a onClick={onClickLink} href={result.nps_link}>
+          {result.title.snippet}
+        </a>
+      </h3>
+    </div>
+    <div className="sui-result__body">
+      {/* use 'raw' values of fields to access values without snippets */}
+      <div className="sui-result__image">
+        <img src={result.image_url} alt="" />
+      </div>
+      {/* Use the 'snippet' property of fields with dangerouslySetInnerHtml to render snippets */}
+      <div
+        className="sui-result__details"
+        dangerouslySetInnerHTML={{ __html: result.description.snippet }}
+      ></div>
+    </div>
+  </li>
+);
+
 export default function App() {
   return (
     <SearchProvider config={config}>
@@ -50,13 +90,24 @@ export default function App() {
             <div className="App">
               <ErrorBoundary>
                 <Layout
-                  header={<SearchBox debounceLength={0} />}
-                  sideContent={<div></div>}
+                  header={<SearchBox autocompleteSuggestions={true}  />}
+                  sideContent={<div>
+                      {wasSearched && (
+                          <Sorting
+                            label={"Sort by"}
+                            sortOptions={buildSortOptionsFromConfig()}
+                          />
+                        )}
+                        {getFactFields.map((field: any, key: any) => (
+                          <Facet key={key} field={field} label={field} />
+                        ))}
+                    </div>}
                   bodyContent={
                     <Results
                       titleField="title"
                       urlField="nps_link"
                       thumbnailField="image_url"
+                      // resultView={CustomResultView}
                       shouldTrackClickThrough
                     />
                   }

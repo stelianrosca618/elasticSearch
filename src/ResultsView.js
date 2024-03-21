591833fafbe357f6a7cb552fed38b0e0dbec4fc3
stelianrosca618 <stelianrosca618@outlook.com>
2024-03-21 19:33:12 +0200

    update read more button

--- src/ResultsView.js
+++ src/ResultsView.js
@@ -1,5 +1,8 @@
+import { useState } from "react"
+
 export const ResultsView = (props) => {
     // console.log(props);
+    const [isShort, setIsShort] = useState(true);
     return (
         <>
             <div className="sui-result">
@@ -10,9 +13,17 @@ export const ResultsView = (props) => {
                     <p className="py-1"><span className="fw-bold">Organisation</span> {props.result.organisation_name.snippet}</p>
                     <p className="py-1"><span className="fw-bold">Applications:</span> {props.result.applications.snippet}</p>
                     <p className="py-1"><span className="fw-bold">Technologies:</span> {props.result.technologies.snippet}</p>
-                    <p className="sui-result__value text-start py-1">
+                    <p className={isShort?"sui-result__value text-start py-1 detail-shortView" : "sui-result__value text-start py-1"}>
                         {props.result.text.raw}
                     </p>
+                    <div className="d-flex justify-content-end">
+                        {isShort && 
+                        <span className="viewMode-btn" onClick={() => setIsShort(false)}>Read More</span>
+                        }
+                        {!isShort && 
+                        <span className="viewMode-btn" onClick={() => setIsShort(true)}>Short View</span>
+                        }
+                    </div>
                 </div>
             </div>
         </>
